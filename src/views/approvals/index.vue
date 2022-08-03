3e4f88b8c20e0ca9aee80f36ceb099f4ce7a97eb
TCIano <740409310@qq.com>
2022-08-03 16:43:34 +0800

    基本骨架

---
+++ src/views/approvals/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>shenpi</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+
+  created() {},
+
+  methods: {},
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/permission/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- <pre></pre> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+
+  created() {},
+
+  methods: {},
+}
+</script>
+
+<style scoped></style>
